f1dddad1da4de9cf20172619c47973400e1f0a35
JulianOviedo <julianovie234@gmail.com>
2023-09-21 11:12:14 -0300

    eslint config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css'
 
-function App() {
+function App () {
   return (
     <>
       <h1>graphql</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
