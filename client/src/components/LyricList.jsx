bb36e465ddcc8626044a27510f612db3f19cfb9f
JulianOviedo <julianovie234@gmail.com>
2023-09-22 13:36:33 -0300

    handleLike

--- client/src/components/LyricList.jsx
+++ client/src/components/LyricList.jsx
@@ -1,4 +1,18 @@
+import { gql, useMutation } from '@apollo/client'
+
 export const LyricList = ({ lyricsArray, songTitle }) => {
+  const [addLike] = useMutation(ADD_LIKE)
+
+  const handleLike = (lyricId) => {
+    addLike({ variables: { id: lyricId } })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <>
       {lyricsArray && (
@@ -11,6 +25,7 @@ export const LyricList = ({ lyricsArray, songTitle }) => {
                   <br />
                   <small>
                     <i>{lyric.likes}</i>
+                    <button onClick={() => handleLike(lyric.id)}>Like</button>
                   </small>
                   <br />
                 </li>
@@ -22,3 +37,12 @@ export const LyricList = ({ lyricsArray, songTitle }) => {
     </>
   )
 }
+
+const ADD_LIKE = gql`
+  mutation AddLike($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`
--- client/src/components/SongDetails.jsx
+++ client/src/components/SongDetails.jsx
@@ -33,6 +33,8 @@ query SongDetails($id: ID!) {
       title
       lyrics {
         content
+        id
+        likes
       }
     }
   }
